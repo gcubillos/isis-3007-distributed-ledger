f7ce2cac615b214a289f5839eb76c8a0e038e18d
Gabriel <g.cubillosb@uniandes.edu.co>
2020-11-03 15:25:02 -0500

    Agrega condiciones state transition

--- data-structures/blockchain-ghost/ghost.go
+++ data-structures/blockchain-ghost/ghost.go
@@ -9,7 +9,7 @@ Containing the blocks and the initial state
  */
 type ghost struct {
 	blocks []block
-	state map[int]account
+	state map[int][]account
 }
 
 // What a block in the network contains
@@ -30,14 +30,21 @@ type block struct {
 // A transaction is a request to move $X from A to B
 type transaction struct {
 	origin string
+	senderSignature string
 	destination string
-	amount float32
+	value float32
+
+
 }
 
 // What an account contains
+// Nonce counter used to make sure each transaction can only be processed once
+// account's current balance
 type account struct{
+
 	nonce int
 	balance float32
+	address string
 }
 
 // *** Functions ***
@@ -73,15 +80,35 @@ func checkUncleValidity(pBlock block) (isValid bool) {
 
 // State transition function. Checks validity of a change in state from a list of transactions
 // Syntax APPLY(S,TX) -> S'
-func stateTransition (pState map[string]int, pTransaction transaction) (isValid bool){
+func stateTransition (pCurrentState map[string]*account, pTransaction transaction) (pModifiedState map[string]*account , err string){
 	// If referenced UTXO  is not in S
-	if {}
+	err = ""
+	pModifiedState = pCurrentState
+	if pCurrentState[pTransaction.senderSignature].balance <= pTransaction.value {
+		err = "The referenced UTXO is not in the state\n"
+	}
+	// If the provided signature does not match the owner of the UTXO
+	if pTransaction.origin != pTransaction.senderSignature {
+		err = err + "The provided signature does not match the owner of the UTXO\n"
+	}
+	// If the sum of the denominations If the sum of the denominations of all input UTXO is less than the sum of the
+	// denominations of all output UTXO, return an error.
+	// TODO: Decide whether to include several inputs and outputs
+	//if pTransaction
+
+	// Return S'
+	if err == "" {
+		pModifiedState[pTransaction.origin].balance -= pModifiedState[pTransaction.origin].balance - 1
+
+	}
+
+
 }
 
 // *** Execution of small scale tests ***
 func main(){
 	// Creating network with no blocks and capacity 1
-	var testGhost = ghost{make([]block,0,1),make(map[int]account)}
+	var testGhost = ghost{make([]block,0,1),make(map[int][]account)}
 	/* Creating the genesis block with the starting parameters for the network
 	 */
 	var genesisBlock =
