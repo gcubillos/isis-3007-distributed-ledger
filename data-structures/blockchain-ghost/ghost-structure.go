0c008bf0d39520a342579f6abf67bfdea700b20e
Gabriel <g.cubillosb@uniandes.edu.co>
2021-01-10 19:40:19 +0200

    Cambia manejo errores

--- data-structures/blockchain-ghost/ghost-structure.go
+++ data-structures/blockchain-ghost/ghost-structure.go
@@ -1,5 +1,7 @@
 package ghost
 
+import "errors"
+
 // *** Structs ***
 /* Declaration of structure
 Contains blocks and state
@@ -14,12 +16,12 @@ type ghost struct {
 
 // State transition function. Checks validity of a change in state from a list of transactions
 // Syntax APPLY(S,TX) -> S'
-func stateTransition(pCurrentState map[string]*account, pTransaction transaction) (pModifiedState map[string]*account, err string) {
+func stateTransition(pCurrentState map[string]*account, pTransaction transaction) (pModifiedState map[string]*account, err error) {
 	// If referenced UTXO is not in S
-	err = ""
+	// TODO: Modify error handling
 	pModifiedState = pCurrentState
 	if pCurrentState[pTransaction.origin].balance <= pTransaction.value {
-		err = "The referenced UTXO is not in the state\n"
+		err = errors.New("The referenced UTXO is not in the state")
 	}
 	// If the provided signature does not match the owner of the UTXO
 	// TODO: Calculating signature
@@ -30,11 +32,11 @@ func stateTransition(pCurrentState map[string]*account, pTransaction transaction
 	// denominations of all output UTXO, return an error. Not necessary given that a
 	// transaction struct only contains one transaction.
 	// Creating the account in the state if it doesn't already exist
-	if _, ok := pModifiedState[pTransaction.destination]; !ok && err == "" {
+	if _, ok := pModifiedState[pTransaction.destination]; !ok && err == nil {
 		CreateAccount(pTransaction.destination)
 	}
 	// Return S'. Apply the changes in the transaction
-	if err == "" {
+	if err == nil {
 		pModifiedState[pTransaction.origin].balance -= pTransaction.value
 		pModifiedState[pTransaction.destination].balance += pTransaction.value
 	}
